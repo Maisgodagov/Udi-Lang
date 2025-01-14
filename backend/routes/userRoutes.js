c63488b49b3096096e365007e3d41dda3ff5da21
mais <mgodagov@vk.com>
2025-01-15 00:34:30 +0300

    ergeer

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,7 +8,6 @@ const router = express.Router();
 // Используем getProfile как обработчик для маршрута
 router.get('/profile', authMiddleware, getProfile);  // Прокачиваешь запрос на правильный обработчик
 
-router.get('/users', authMiddleware, getUsers);
 router.get(
     '/users',
     authMiddleware,
