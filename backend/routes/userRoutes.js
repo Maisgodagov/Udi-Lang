b1141746a2cdc394847dfc1abcf22e71fde933ea
mais <mgodagov@vk.com>
2025-01-28 14:41:05 +0300

    iejhio

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -22,4 +22,4 @@ router.put(
 // GET /api/user/stats
 router.get('/stats', authMiddleware, getUserStats);
   
-module.exports = router; // Экспортируем router наsпрямую
+module.exports = router; // Экспортируем router н8eаsпрямую
